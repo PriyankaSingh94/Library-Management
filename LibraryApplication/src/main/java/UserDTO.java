c67f3ffd7253fde4513e6f842fc87155ff45f5b2
PriyankaSingh94 <raopriyankasingh94@gmail.com>
2018-06-10 16:00:17 +0530

    Posted library project to github.

---
+++ LibraryApplication/src/main/java/UserDTO.java
@@ -0,0 +1,36 @@
+package main.java;
+
+import java.sql.Timestamp;
+
+public class UserDTO {
+	private String userName;
+	private String password;
+	private String roleName;
+	private Timestamp lastLogin;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	public Timestamp getLastLogin() {
+		return lastLogin;
+	}
+	public void setLastLogin(Timestamp lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+	
+}
